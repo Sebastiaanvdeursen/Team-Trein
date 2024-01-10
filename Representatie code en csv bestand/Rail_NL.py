2dead4113d88111c1c041fbe1c23dbd6970e0d39
unknown <sebasvandeursen@outlook.com>
2024-01-10 20:02:05 +0100

    Made a random algorithm for the problem

--- Representatie code en csv bestand/Rail_NL.py
+++ Representatie code en csv bestand/Rail_NL.py
@@ -1,4 +1,5 @@
 import csv
+import random
 from station import Station
 from traject import Traject
 
@@ -38,24 +39,62 @@ class Rail_NL:
         traject = Traject(starting_station, rail_instance)
         return traject
 
-# Main script
-if __name__ == "__main__":
-    from Rail_NL import Rail_NL
-
+def run_random_traject():
     Noord_Holland = Rail_NL()
 
-    # Toon verbindingen
-    Noord_Holland.display_connections()
+    list_stations = []
 
-    traject_1 = Noord_Holland.create_traject("Amsterdam Zuid", Noord_Holland)
+    for station_name in Noord_Holland.stations:
+        list_stations.append(station_name)
+    
+    random_number = random.randint(0, 21)
 
-    traject_1.show_current_traject()
+    random_traject = Noord_Holland.create_traject(list_stations[random_number], Noord_Holland)
 
-    traject_1.move("Schiphol Airport")
+    while True:
+        if random_traject.total_time + 36 > 120:
+            break
+        list_stations_current = []
+        for station_name in random_traject.current_station.connections:
+            list_stations_current.append(station_name)
+        random_number = random.randint(0, len(random_traject.current_station.connections) - 1)
+        random_traject.move(list_stations_current[random_number])
+        random_int_2 = random.randint(0, 9)
+        if random_int_2 == 9:
+            break
+    random_traject.show_current_traject()
 
-    traject_1.move("Leiden Centraal")
+    time = random_traject.total_time
+    return [time, Noord_Holland]
 
-    traject_1.show_current_traject()
+def run_random_amount_of_trajects():
+    random_number = random.randint(1, 7)
+    time = []
+    for i in range(1, random_number):
+        time.append(run_random_traject()[0])
+    print(sum(time))
 
-    Noord_Holland.display_connections()
+    n_done = 0
+    n_not_done = 0
+    for station in run_random_traject()[1].stations.values():
+            for connection in station.connections.values():
+                if connection.done == False:
+                    n_not_done += 1
+                else:
+                    n_done += 1
+    
+    fraction_done = n_done / (n_not_done + n_done)
+    return sum(time), random_number, fraction_done
+
+
+# Main script
+if __name__ == "__main__":
+    Min, T, p = run_random_amount_of_trajects()
+    print(Min, T, p)
+    K = p*10000 - (T*100 + Min)
+    print(K)
+                
+
+
+    
 
