506b5c1b2ecbf1d204f1fab12d06650e565fdd3d
unknown <sebasvandeursen@outlook.com>
2024-01-11 13:43:42 +0100

    Got the check50 working

--- Representatie code en csv bestand/Rail_NL.py
+++ Representatie code en csv bestand/Rail_NL.py
@@ -3,10 +3,26 @@ import random
 from station import Station
 from traject import Traject
 
+random.seed(8)
+
 class Rail_NL:
     def __init__(self):
         self.stations = {}
         self.load_connections("ConnectiesHolland.csv")
+    
+    
+    def load_connections(self, filename):
+        with open(filename) as csv_bestand:
+            csv_read = csv.reader(csv_bestand, delimiter=',')
+            line_count = 0
+            for row in csv_read:
+                if line_count != 0:
+                    self.add_station(row[0])
+                    self.add_station(row[1])
+                    self.add_connection(row[0], row[1], int(row[2]))
+                    self.add_connection(row[1], row[0], int(row[2]))
+                line_count += 1
+        self.total_connections = line_count - 1
 
     def add_station(self, station_name):
         if station_name not in self.stations:
@@ -22,34 +38,21 @@ class Rail_NL:
                 status = "Done" if connection.done else "Not Done"
                 print(f"  To: {destination}, Time: {connection.time} minutes, Status: {status}")
 
-    def load_connections(self, filename):
-        with open(filename) as csv_bestand:
-            csv_read = csv.reader(csv_bestand, delimiter=',')
-            line_count = 0
-            for row in csv_read:
-                if line_count != 0:
-                    self.add_station(row[0])
-                    self.add_station(row[1])
-                    self.add_connection(row[0], row[1], int(row[2]))
-                    self.add_connection(row[1], row[0], int(row[2]))
-                line_count += 1
 
     def create_traject(self, starting_station_name, rail_instance):
         starting_station = self.stations[starting_station_name]
         traject = Traject(starting_station, rail_instance)
         return traject
 
-def run_random_traject():
-    Noord_Holland = Rail_NL()
-
+def run_random_traject(Area):
     list_stations = []
 
-    for station_name in Noord_Holland.stations:
+    for station_name in Area.stations:
         list_stations.append(station_name)
     
     random_number = random.randint(0, 21)
 
-    random_traject = Noord_Holland.create_traject(list_stations[random_number], Noord_Holland)
+    random_traject = Area.create_traject(list_stations[random_number], Area)
 
     while True:
         if random_traject.total_time + 36 > 120:
@@ -63,35 +66,38 @@ def run_random_traject():
         if random_int_2 == 9:
             break
 
+
     random_traject.show_current_traject()
 
     time = random_traject.total_time
-    return [time, Noord_Holland]
+    return [time, Area]
 
-def run_random_amount_of_trajects():
+
+
+
+def run_random_amount_of_trajects(Area):
     random_number = random.randint(1, 7)
 
     time = []
-    for i in range(1, random_number):
-        time.append(run_random_traject()[0])
+    for i in range(0, random_number):
+        time.append(run_random_traject(Area)[0])
 
     n_done = 0
     n_not_done = 0
-    for station in run_random_traject()[1].stations.values():
+    for station in Area.stations.values():
             for connection in station.connections.values():
-                if connection.done == False:
-                    n_not_done += 1
-                else:
+                if connection.done == True:
                     n_done += 1
     
-    fraction_done = n_done / (n_not_done + n_done)
+    fraction_done = (n_done / 2) / Area.total_connections
     return sum(time), random_number, fraction_done
 
 
 # Main script
 if __name__ == "__main__":
+    Noord_Holland = Rail_NL()
     print("train,stations")
-    Min, T, p = run_random_amount_of_trajects()
+    Min, T, p = run_random_amount_of_trajects(Noord_Holland)
     K = p*10000 - (T*100 + Min)
     print(f"score,{K}")
                 
--- Representatie code en csv bestand/traject.py
+++ Representatie code en csv bestand/traject.py
@@ -4,18 +4,20 @@ class Traject:
     train_count = 0
 
     def __init__(self, starting_station, rail_instance):
-        self.connections = []
         self.total_time = 0
         self.current_station = starting_station
+        self.connections = [starting_station.name]
         self.rail_instance = rail_instance
         Traject.train_count += 1
 
     def move(self, destination):
         connection = self.current_station.connections[destination]
+        connection2 = self.rail_instance.stations[destination].connections[self.current_station.name]
 
         assert self.total_time + connection.time <= 120, "Went over time limit of 2 hours"
 
         connection.done = True
+        connection2.done = True
 
         self.connections.append(connection.destination)
         self.total_time += connection.time
