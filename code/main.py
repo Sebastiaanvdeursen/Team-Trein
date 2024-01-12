c2cb2710124ecf74f99429700dc43127d6376fd7
mathijs leons <mathijs@leons-fam.nl>
2024-01-12 13:06:22 +0100

    added greedy algorithm

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 from random_alg import run_random_amount_of_trajects
 from rail_NL import Rail_NL
 import sys
-
+from greedy_random_start import run_greedy_random
 
 # Main script
 if __name__ == "__main__":
@@ -27,6 +27,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         if sys.argv[2] == "random":
             Min, T, p = run_random_amount_of_trajects(area, amount_trajects, max_time, amount_stations)
+            K = p*10000 - (T*100 + Min)
+            print(f"score,{K}")
+        elif sys.argv[2] == "greedy_random":
+            Min, T, p = run_greedy_random(area, amount_trajects, max_time, amount_stations)
             K = p*10000 - (T*100 + Min)
             print(f"score,{K}")
         else:
