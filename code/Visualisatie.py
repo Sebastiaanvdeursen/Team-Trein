7e9d24c21120f6d8f3f8750ded292f3da2622c26
mathijs leons <mathijs@leons-fam.nl>
2024-01-12 11:11:20 +0100

    cleaned up Visualisatie.py

--- code/Visualisatie.py
+++ code/Visualisatie.py
@@ -3,18 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "large":
-            coords = "Long"
-            lines = "NL"
-        else:
-            coords = "Short"
-            lines = "Holland"
-    else:
-            coords = "Short"
-            lines = "Holland"
-
+def scatterplot(coords):
     places = {}
     with open(f"../data/coordinates{coords}.csv") as data:
         csv_read = csv.reader(data, delimiter=',')
@@ -31,7 +20,10 @@ if __name__ == "__main__":
         l_y.append(places[a][0])
 
     plt.scatter(l_x, l_y)
+    return places
 
+
+def draw_lines(lines, places):
     with open(f"../data/Connecties{lines}.csv") as line_info:
         csv_file = csv.reader(line_info, delimiter=',')
         line_count = 0
@@ -42,6 +34,23 @@ if __name__ == "__main__":
                 print(places[row[1]])
                 plt.plot([places[row[0]][1], places[row[1]][1]], [places[row[0]][0], places[row[1]][0]], 'k-', lw=2)
             line_count += 1
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "large":
+            coords = "Long"
+            lines = "NL"
+        else:
+            coords = "Short"
+            lines = "Holland"
+    else:
+            coords = "Short"
+            lines = "Holland"
+
+    places = {}
+    places = scatterplot(coords)
+    draw_lines(lines, places)
 
     if coords == "Short":
         plt.ylim(51.7, 53.5)
