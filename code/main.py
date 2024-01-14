12995b445772e29ea41a3174819edaf321e33e8d
unknown <sebasvandeursen@outlook.com>
2024-01-14 13:09:13 +0100

    added the optimized random algorithm and made main working again

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 from random_alg import run_random_amount_of_trajects
+from random_alg_opt import run_random_amount_of_trajects_opt
 from rail_NL import Rail_NL
 import sys
 from greedy_random_start import run_greedy_random
@@ -25,16 +26,20 @@ if __name__ == "__main__":
 
     area = Rail_NL(map, amount_trajects, amount_stations, max_time)
     print("train,stations")
-    if len(sys.argv) > 2:
-        if sys.argv[2] == "random":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "random":
             Min, T, p = run_random_amount_of_trajects(area, amount_trajects, max_time, amount_stations)
             K = p*10000 - (T*100 + Min)
             print(f"score,{K}")
-        elif sys.argv[2] == "greedy_random":
+        elif sys.argv[1] == "random_opt":
+            Min, T, p = run_random_amount_of_trajects_opt(area, amount_trajects, max_time, amount_stations)
+            K = p*10000 - (T*100 + Min)
+            print(f"score,{K}")
+        elif sys.argv[1] == "greedy_random":
             Min, T, p = run_greedy_random(area, amount_trajects, max_time, amount_stations)
             K = p*10000 - (T*100 + Min)
             print(f"score,{K}")
-        elif sys.argv[2] == "greedy_optim":
+        elif sys.argv[1] == "greedy_optim":
             run_greedy_combinations(map, amount_trajects, max_time, amount_stations)
         else:
             print("usage python3 main.py size algorithm")
--- code/random_alg_opt.py
+++ code/random_alg_opt.py
@@ -4,7 +4,7 @@ import sys
 from station import Station
 from traject import Traject
 
-def run_random_traject(Area, amount_stations, max_time):
+def run_random_traject_opt(Area, amount_stations, max_time):
     list_stations = []
 
     for station_name in Area.stations:
@@ -16,15 +16,18 @@ def run_random_traject(Area, amount_stations, max_time):
 
     while True:
         list_stations_current = []
+        list_stations_current_not_done = []
         for station_name in random_traject.current_station.connections:
+            if random_traject.current_station.connections[station_name].done == False:
+                list_stations_current_not_done.append(station_name)
             list_stations_current.append(station_name)
-        random_number = random.randint(0, len(random_traject.current_station.connections) - 1)
-        if random_traject.total_time + random_traject.current_station.connections[list_stations_current[random_number]].time > max_time:
+        if len(list_stations_current_not_done) == 0:
             break
-        random_traject.move(list_stations_current[random_number])
-        random_int_2 = random.randint(0, 9)
-        if random_int_2 == 9:
+
+        random_number = random.randint(0, len(list_stations_current_not_done) - 1)
+        if random_traject.total_time + random_traject.current_station.connections[list_stations_current_not_done[random_number]].time > max_time:
             break
+        random_traject.move(list_stations_current_not_done[random_number])
 
 
     random_traject.show_current_traject()
@@ -35,12 +38,12 @@ def run_random_traject(Area, amount_stations, max_time):
 
 
 
-def run_random_amount_of_trajects(Area, amount_trajects, max_time, amount_stations):
+def run_random_amount_of_trajects_opt(Area, amount_trajects, max_time, amount_stations):
     random_number = random.randint(1, amount_trajects)
 
     time = []
     for i in range(0, random_number):
-        time.append(run_random_traject(Area, amount_stations, max_time)[0])
+        time.append(run_random_traject_opt(Area, amount_stations, max_time)[0])
 
     n_done = 0
     for station in Area.stations.values():
