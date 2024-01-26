a9dc3ab497e27aea23e016077e8253181d73f0ee
mathijs leons <mathijs@leons-fam.nl>
2024-01-26 14:25:02 +0100

    added code to check the effect of deleting a line

--- code/classes/rail_NL.py
+++ code/classes/rail_NL.py
@@ -19,59 +19,27 @@ class Rail_NL:
     def load_connections(self, filename):
         remove = ""
         if self.removing != "":
-            if self.removing =="Utrecht":
-                remove ="Utrecht Centraal"
-            elif self.removing =="Almere":
-                remove ="Almere Centrum"
-            elif self.removing =="Amstel":
-                remove ="Amsterdam Amstel"
-            elif self.removing =="Centraal":
-                remove ="Amsterdam Centraal"
-            elif self.removing =="Sloterdijk":
-                remove ="Amsterdam Sloterdijk"
-            elif self.removing =="Zuid":
-                remove ="Amsterdam Zuid"
-            elif self.removing =="Arnhem":
-                remove ="Amsterdam Centraal"
-            elif self.removing =="Den_haag":
-                remove ="Den Haag Centraal"
-            elif self.removing == "NOI":
-                remove = "Den Haag Laan v NOI"
-            elif self.removing == "HS":
-                remove = "Den Haag HS"
-            elif self.removing == "leiden":
-                remove = "Leiden Centraal"
-            elif self.removing == "Alexander":
-                remove = "Rotterdam Alexander"
-            elif self.removing == "Rotterdam":
-                remove = "Rotterdam Centraal"
-            elif self.removing == "Blaak":
-                remove = "Rotterdam Blaak"
-            elif self.removing == "Schiedam":
-                remove = "Schiedam Centrum"
-            else:
-                remove = self.removing
-
+            remove = self.remove_station()
         with open(filename) as csv_bestand:
             csv_read = csv.reader(csv_bestand, delimiter=',')
             line_count = 0
             if self.randomizer:
                 station_names = []
-                numbers = []
+                self.numbers = []
                 if map == "small":
                     amount_connections = 28
                 else:
                     amount_connections = 89
                 for _ in range(3):
-                    numbers.append(random.randint(2, amount_connections))
+                    self.numbers.append(random.randint(2, amount_connections))
             for row in csv_read:
                 if line_count != 0:
                     if self.randomizer:
                         station_names.append(row[0])
                         self.add_station(row[0])
-                        if line_count in numbers:
+                        if line_count in self.numbers:
                             destination = random.choice(station_names)
-                            time = random.randint(1, 40)
+                            time = random.randint(5, 70)
                             print(f"removed[{row[0]}, {row[1]}], {int(float(row[2]))}")
                             print(f"added: [{row[0]}, {destination}], {time}")
                             self.add_connection_stations(row[0], destination, time)
@@ -123,4 +91,41 @@ class Rail_NL:
         self.amount_stations = len(self.stations)
         return self.amount_stations
 
+    def remove_station(self):
+        remove = ""
+        if self.removing =="Utrecht":
+                remove ="Utrecht Centraal"
+        elif self.removing =="Almere":
+                remove ="Almere Centrum"
+        elif self.removing =="Amstel":
+            remove ="Amsterdam Amstel"
+        elif self.removing =="Centraal":
+                remove ="Amsterdam Centraal"
+        elif self.removing =="Sloterdijk":
+                remove ="Amsterdam Sloterdijk"
+        elif self.removing =="Zuid":
+                remove ="Amsterdam Zuid"
+        elif self.removing =="Arnhem":
+                remove ="Amsterdam Centraal"
+        elif self.removing =="Den_haag":
+            remove ="Den Haag Centraal"
+        elif self.removing == "NOI":
+            remove = "Den Haag Laan v NOI"
+        elif self.removing == "HS":
+            remove = "Den Haag HS"
+        elif self.removing == "leiden":
+            remove = "Leiden Centraal"
+        elif self.removing == "Alexander":
+            remove = "Rotterdam Alexander"
+        elif self.removing == "Rotterdam":
+            remove = "Rotterdam Centraal"
+        elif self.removing == "Blaak":
+            remove = "Rotterdam Blaak"
+        elif self.removing == "Schiedam":
+            remove = "Schiedam Centrum"
+        else:
+            remove = self.removing
+        return remove
 
+    def get_ids(self):
+         return self.numbers
