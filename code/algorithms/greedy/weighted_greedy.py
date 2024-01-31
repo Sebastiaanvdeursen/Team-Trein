5cadf339ec668cae4687a0626796176f3090621c
Ties Veltman <tiesveltmannn@gmail.com>
2024-01-31 13:03:09 +0100

    Fixed redundant code

--- code/algorithms/greedy/weighted_greedy.py
+++ code/algorithms/greedy/weighted_greedy.py
@@ -7,7 +7,7 @@ from code.classes.rail_NL import Rail_NL
 from code.other.remove_unnecessary import removing_lines
 from code.other.run import run_trajects
 
-def run_weighted(Area, amount_trajects, max_time, amount_stations, printed = True, info = False, power = 1.6):
+def run_weighted(Area, amount_trajects, max_time, amount_stations, printed: bool = True, info: bool = False, power = 1.6):
     list_stations = []
 
     for station_name in Area.stations:
